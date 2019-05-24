039f8b306cc865c56c5a5082ff7b487e29481956
Heather Smith <heathersmith@google.com>
2019-05-24 21:39:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ function currentLine(katzDeliLine) {
   if (lineLength === 0) {
     return "The line is currently empty."
   }
-  
-
+  var outputString = "The line is currently: "
+  var position = 1;
+  for (var element of katzDeliLine) {
+    outputString += position+". "+element+", "
+    position++
+  }
+  return outputString
 }
