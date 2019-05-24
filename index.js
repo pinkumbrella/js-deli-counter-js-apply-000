eb817ba0390b86a90ce2222c66d735b9eca09d77
Heather Smith <heathersmith@google.com>
2019-05-24 21:00:23 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+var katzDeliLine = [];
+var number = 0
+function takeANumber(name, katzDeliLine) {
+  katzDeliLine.push(name)
+  return "Welcome, "+name+". You are number"+katzDeliLine.length+" in line."
+}
+
+function nowServing(katzDeliLine) {
+  return katzDeliLine[0]
+}
+
+function currentLine(katzDeliLine)
