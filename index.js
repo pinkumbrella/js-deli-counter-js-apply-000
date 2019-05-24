f47334ca099336d6bce26509e0691a6b42c1080f
Heather Smith <heathersmith@google.com>
2019-05-24 21:32:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(katzDeliLine) {
-  if (katz.length === 0) {
+  if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   }
   return "Currently serving "+katzDeliLine.unshift()+"."
